173a6ecc25089b154b603452df858f8546f0231e
Paul Laskowski <pelaskowski@gmail.com>
2015-08-17 11:50:57 -0700

    connection to google apps spreadsheet data added package ongoworks:google-spreadsheets setup server to create cache of data

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,8 @@
 Meteor.startup(function () {
   // code to run on server at startup
+  Meteor.startup(function () {
+    Meteor.setInterval(function() {
+      Meteor.call('spreadsheet/fetch', '0AicrD4kSOriScEpzNXMxakRadVZUTnFjMVBhZXVvV0E', 'od6', 'R5C1:R275C21', 5)
+    },50000);
+  });
 });
