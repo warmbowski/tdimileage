4dee3c10270acfaaa6baa5c7e2c72f3fd0b72bbe
Paul Laskowski <pelaskowski@gmail.com>
2015-08-23 22:43:03 -0700

    removed blaze hello template and helpers

--- client/app.jsx
+++ client/app.jsx
@@ -2,19 +2,3 @@ Meteor.startup(function () {
   // code to run on client at startup
   React.render(<AppParent />, document.getElementById('app-container'));
 });
-
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
-});
-
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
-});
