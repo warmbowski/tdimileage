bc6c1e4c7fe39f33fcae40cd87cdf544b14e0f79
Paul Laskowski <pelaskowski@gmail.com>
2015-08-24 09:22:30 -0700

    fixed meteor method call to retrieve data and headers
    google-spreadsheet package has bug declaring worksheet (should be number, not string)

--- server/startup.js
+++ server/startup.js
@@ -2,7 +2,7 @@ Meteor.startup(function () {
   // code to run on server at startup
   Meteor.startup(function () {
     Meteor.setInterval(function() {
-      Meteor.call('spreadsheet/fetch', '0AicrD4kSOriScEpzNXMxakRadVZUTnFjMVBhZXVvV0E', 'od6', 'R5C1:R275C21', 5)
+      Meteor.call('spreadsheet/fetch', '0AicrD4kSOriScEpzNXMxakRadVZUTnFjMVBhZXVvV0E', '0', 'R4C1:R275C21', 4)
     },50000);
   });
 });
